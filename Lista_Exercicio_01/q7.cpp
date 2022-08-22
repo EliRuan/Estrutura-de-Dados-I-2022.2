f856b06314b12b3026d44fd8ce945524e960d5ba
Eli Ruan Carvalho <61792997+EliRuan@users.noreply.github.com>
2022-08-22 11:52:11 -0300

    Add files via upload

---
+++ Lista_Exercicio_01/q7.cpp
@@ -0,0 +1,17 @@
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+	
+	float valor_reais, novo_valor;
+	printf("Digite um valor em Reais (R$): ");
+	scanf("%f", &valor_reais);
+	
+	novo_valor = valor_reais * 0.7;
+			
+	printf("70%% de %0.2f eh %0.2f", valor_reais, novo_valor); 
+	return 0;
+}
+
